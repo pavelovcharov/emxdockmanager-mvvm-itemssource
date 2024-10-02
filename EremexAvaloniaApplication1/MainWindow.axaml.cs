431817943158aba4d7cdf7bfa7426ddc072d871d
pavelovcharov <1357165+pavelovcharov@users.noreply.github.com>
2024-10-02 15:03:59 +0300

    Add project files.

---
+++ EremexAvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+using Eremex.AvaloniaUI.Controls.Common;
+
+namespace EremexAvaloniaApplication1
+{
+    public partial class MainWindow : MxWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EremexAvaloniaApplication1/emxLicense.cs
@@ -0,0 +1,16 @@
+﻿// This file is auto-generated. Please do not change it.
+using System.Runtime.CompilerServices;
+
+using Eremex.AvaloniaUI.Controls.License;
+
+namespace EremexAvaloniaApplication1;
+public class LicenseProvider
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+	[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void RegisterLicense()
+	{
+        ControlsLicenseManager.SetRuntimeLicenseOwner(new LicenseProvider(),"", "A7 E0 2F 71 39 93 76 EC 1E B5 9C 2A F9 32 76 07 6A 2A 58 A1 36 6A 37 84 2B F7 6C 8D 5F 93 54 22 9F EA 79 24 C5 44 73 6F");
+	}
+}
